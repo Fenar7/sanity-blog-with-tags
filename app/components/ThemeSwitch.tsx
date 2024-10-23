b8e25c83e916ffe67e9f9b50a68b6f2f164e37e0
Fenar Khan <fenar@Fenars-Mac-Pro.local>
2024-10-23 09:31:36 +0530

    jksjf

--- app/components/ThemeSwitch.tsx
+++ app/components/ThemeSwitch.tsx
@@ -1,8 +1,22 @@
+"use client";
+
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
 
 const ThemeSwitch = () => {
+    const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(()=>{
+      setMounted(true)
+    },[])
+
+  if(!mounted){
+    return null
+  }
   return (
-    <button>Theme Switch</button>
+    <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>Theme Switch</button>
   )
 }
 
